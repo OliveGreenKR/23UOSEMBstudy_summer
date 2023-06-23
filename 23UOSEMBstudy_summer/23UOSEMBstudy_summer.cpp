748f08039b82aceb8228405e43b66dbf1dd30ad0
OliveGreen <olivegreenkr98@gmail.com>
2023-06-23 21:18:19 +0900

    ch04.02 program structualize

--- 23UOSEMBstudy_summer/23UOSEMBstudy_summer.cpp
+++ 23UOSEMBstudy_summer/23UOSEMBstudy_summer.cpp
@@ -27,7 +27,7 @@ double grade(double midterm, double final, vector<double>& homeworks) {
 }
 
 //스트림을 이용해 숙제 점수를 입력받아서 벡터에 저장
-istream& read_hw(istream& in, OUT vector<double>hws) {
+istream& read_hw(istream& in, OUT vector<double>& hws) {
     if (in) {
         
         hws.clear(); // make empty
@@ -42,73 +42,35 @@ istream& read_hw(istream& in, OUT vector<double>hws) {
     return in;
 }
 
-
 int main()
 {
     ios_base::sync_with_stdio(false); //FASTIO , dont use printf,scanf anymore
 
-    //grade(0,0,vector<double>())
-    //error - not lvalue
-
-	/*
-	* lvalue(왼값)          : 비일시적 값.
-	* rvalue(오른값)        : 왼값이 아닌 나머지. 임시값과 비슷한 의미.
-    * 
-    * 
-    * 왼값 참조 : 원본 접근 및 수정 가능
-    * const 왼값 참조 : read only
-    * 
-    * 오른값 참조 :  원본의 접근 및 수정 가능 + 원본의 휘발성을 나타냄.
-    *               >> 대상의 '이동' 에 자주 이용된다. 대상의 전체 복사 없이, 적은 리소스로 값을 '이동' 시킬 수 있음.
-    *
-    */
-
-    class Knight {
-    public:
-        Knight(int hp, int mp) {
-            _hp = hp;
-            _mp = mp;
-        }
-
-        void operator= (Knight&& k) noexcept {
-            //얕은 복사
-            _hp = k._hp;
-            _mp = k._mp;
-        }
-        int _hp;
-        int _mp;
-    };
-
-
-    auto k1 =  new Knight(100,100);
-    auto k2 =  new Knight(20,20);
+    string name;
+    cout << "student name : ";
+    cin >> name;
     
-    cout << k2->_hp  << endl;
-    k2 = k1;
+    double midterm, final;
+    cout << "midterm and final : ";
+    cin >> midterm >> final; 
 
-    cout << k2->_hp << endl;
-    cout << k1->_hp << endl;
+    vector<double> hw;
+    cout << "homeworks followd by EOF : ";
+    read_hw(::cin, hw);
 
-
-    /* 전달 참조(forwarding reference) :  타입 연역(deduction)이 발생할 때  보편적으로 참조하는 것.
-    */ 
-    vector<double> vecs;
-
-    for (auto&& v : vecs) {  //왼값, 오른값 아무거나 받음
-        cout << v << endl;
+    try {
+        double final_grade = grade(midterm, final, hw);
+        streamsize prec = cout.precision(3);
+        cout << "yout final grade is : " << setprecision(3) << final_grade << setprecision(prec) << endl;
+    }
+    catch (domain_error) {
+        cin.clear();
+        cout << "you must enter your homework grades!\n"
+            << "Try Again";
+        return 1; //exit with error
     }
 
-	//for (const auto&& v : vecs) {  //전달 참조 아님.
-    //    cout << v << endl;
-    //}
-
-    Knight&& k5 = Knight(100, 100);
-
-    auto testRvalue = [ ](Knight&& knight) { };
-    testRvalue(::move(k5));
-    //testRvalue(k5);
-
-
+    return 0;
 }
 
 
