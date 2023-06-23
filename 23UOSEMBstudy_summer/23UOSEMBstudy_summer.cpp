a79d4c63c5331cf5973bb46140faeaf0476a7b23
OliveGreen <olivegreenkr98@gmail.com>
2023-06-23 21:09:17 +0900

    ch04. rvlaue

--- 23UOSEMBstudy_summer/23UOSEMBstudy_summer.cpp
+++ 23UOSEMBstudy_summer/23UOSEMBstudy_summer.cpp
@@ -3,75 +3,112 @@
 #include <ios>      //streamsize
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 
-using namespace std;
-//using std::cin;
+#define OUT
 
-bool Greater(const double& A, const double& B) {
-    return A>B;
+using namespace std;
+
+template <typename T = double>
+auto GetAvgFromVec = [](const vector<T>& vec) {  //lamda
+	T sum = 0;
+	for (auto& e : vec)
+		sum += e;
+	return  sum / vec.size();
+};
+
+double grade(double midterm, double final, vector<double>& homeworks) {
+
+    if (homeworks.size() == 0)
+        throw domain_error("student has done no homework");
+
+    return 0.4 * midterm  + 0.4 * final + 0.2 * GetAvgFromVec<double>(homeworks);
 }
+
+//스트림을 이용해 숙제 점수를 입력받아서 벡터에 저장
+istream& read_hw(istream& in, OUT vector<double>hws) {
+    if (in) {
+        
+        hws.clear(); // make empty
+
+        double x;
+        while (in >> x) {
+            hws.push_back(x);
+        }
+
+        in.clear(); //clear stream
+    }
+    return in;
+}
+
 
 int main()
 {
     ios_base::sync_with_stdio(false); //FASTIO , dont use printf,scanf anymore
 
-    // 정보 입력
-    string name;
-    cout << "enter your first name : ";
-    cin >> name; 
+    //grade(0,0,vector<double>())
+    //error - not lvalue
 
-    double midterm, final;
-    cout << "Enter your midterm and final exam grades : ";
-    cin >> midterm >> final;
-
-    vector<double> homeworks;
-    double homework;
-    double sum_hw = 0;
-  
-    cout << "Enter your howework grades , followd by EOF.\n";
-
-    while (cin >> homework) {
-        homeworks.push_back(homework);
-        sum_hw += homework;
-    }
-   
-    //sorting 오름차순
-    ::sort(homeworks.begin(), homeworks.end());
-    
-    for (double hw : homeworks)
-        cout << hw << " ";
-    
-    cout << "\n------------\n";
-
-    //sorting 내림차순
-    ::sort(homeworks.begin(), homeworks.end(),::greater<double>());
-    //::sort(homeworks.begin(), homeworks.end(), [ ](const double& A, const double& B) { return A > B; });
-    //::sort(homeworks.begin(), homeworks.end(), Greater);
-
- 
-
-    for (double hw : homeworks)
-        cout << hw << " ";
-
-    cout << "\n------------\n";
-
-
-
-
-    streamsize prec = cout.precision();
-    cout << "yout final grade is " << setprecision(3)
-        << 0.2 * midterm + 0.4* final + 0.4*(sum_hw / homeworks.size()) << setprecision(prec) << endl;
-
-    /* 
-    streamsize prec = cout.precision(3);   
-    cout << "yout final grade is " << setprecision(3)
-         << 0.2 * midterm + 0.4* final + 0.4*(sum_hw / cnt_hw) << setprecision(prec) << endl;
-    cout.precision(prec);
-    
-    이렇게도 가능하지만, 위의 방법을 추천.
-    why? 아래의 방법은 cout 전체에 적용 -> 프로그램 전체에 영향을 미치므로 위험하다.
+	/*
+	* lvalue(왼값)          : 비일시적 값.
+	* rvalue(오른값)        : 왼값이 아닌 나머지. 임시값과 비슷한 의미.
+    * 
+    * 
+    * 왼값 참조 : 원본 접근 및 수정 가능
+    * const 왼값 참조 : read only
+    * 
+    * 오른값 참조 :  원본의 접근 및 수정 가능 + 원본의 휘발성을 나타냄.
+    *               >> 대상의 '이동' 에 자주 이용된다. 대상의 전체 복사 없이, 적은 리소스로 값을 '이동' 시킬 수 있음.
+    *
     */
+
+    class Knight {
+    public:
+        Knight(int hp, int mp) {
+            _hp = hp;
+            _mp = mp;
+        }
+
+        void operator= (Knight&& k) noexcept {
+            //얕은 복사
+            _hp = k._hp;
+            _mp = k._mp;
+        }
+        int _hp;
+        int _mp;
+    };
+
+
+    auto k1 =  new Knight(100,100);
+    auto k2 =  new Knight(20,20);
+    
+    cout << k2->_hp  << endl;
+    k2 = k1;
+
+    cout << k2->_hp << endl;
+    cout << k1->_hp << endl;
+
+
+    /* 전달 참조(forwarding reference) :  타입 연역(deduction)이 발생할 때  보편적으로 참조하는 것.
+    */ 
+    vector<double> vecs;
+
+    for (auto&& v : vecs) {  //왼값, 오른값 아무거나 받음
+        cout << v << endl;
+    }
+
+	//for (const auto&& v : vecs) {  //전달 참조 아님.
+    //    cout << v << endl;
+    //}
+
+    Knight&& k5 = Knight(100, 100);
+
+    auto testRvalue = [ ](Knight&& knight) { };
+    testRvalue(::move(k5));
+    //testRvalue(k5);
+
+
 }
 
 
