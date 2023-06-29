df4ab40b03dadb0af86e9076635a73f7b536babd
OliveGreen <olivegreenkr98@gmail.com>
2023-06-29 20:51:47 +0900

    deconstructor

--- 23UOSEMBstudy_summer/Vec.h
+++ 23UOSEMBstudy_summer/Vec.h
@@ -12,8 +12,10 @@ public:
 	using const_reference = const T&;
 
 	Vec()										{ create(); }
-	Vec(const Vec& v)							{ create(v.begin(), v.end()); }
+	Vec(const Vec& v)/*복사생성자*/				{ create(v.begin(), v.end()); }
 	explicit Vec(size_t n, const T& val = T())	{ create(n, val); }
+
+	~Vec()										{ uncreate(); }
 
 public:
 	size_type size() const					{ return _limit - _data; }
@@ -43,14 +45,3 @@ private:
 	iterator _data;	
 	iterator _limit;	
 };
-
-/* 할당 연산자 (assignment operator)
-* opertaor= 할당 연산자
-* 클래스 자체의 const_reference 타입의 할당 연산자는 특별하다!
-* 
-* 클래스 타입의 값을 할당하는 것이 무엇을 의미하는지 정의하기 때문.
-* 
-* header내의 template 공간 안에서는 편의성을 위해 '<T>'를 생략해도 되지만, 외부에서 정의를하는 cpp에서는 꼭 명시해주어야한다.
-* 하지만 cpp 에서도 한 함수에서 한번 <T>를 명시하면, 암묵적으로 <T>인 객체로 인정되어 생략가능.
-*/
-
