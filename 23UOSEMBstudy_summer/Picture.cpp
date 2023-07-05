b3b744f8973d43f70c2744f56cc79336c56ed11f
OliveGreen <olivegreenkr98@gmail.com>
2023-07-05 15:17:20 +0900

    interface class

--- 23UOSEMBstudy_summer/Picture.cpp
+++ 23UOSEMBstudy_summer/Picture.cpp
@@ -3,150 +3,181 @@
 #include <cctype>
 #include <algorithm>
 #include <iostream>
+//
+////반복자를 통해 split 확장성 증가시켜보기
+//template < class Out>
+//void split(const string& str, Out os) {
+//	using iter = string::const_iterator;
+//	iter i = str.begin();
+//
+//	auto isSpace = [ ](char c) { return isspace(c); };
+//	auto NotSpace = [ ](char c) { return !isspace(c); };
+//
+//	while (i != str.end()) {
+//
+//		i = find_if(i, str.end(), NotSpace);
+//
+//		iter j = find_if(i, str.end(), isSpace);
+//
+//		if (i != str.end())
+//			*os++ = string(i, j);  //stl의 반복자 특성을 이용.
+//
+//		i = j;
+//	}
+//}
+//
+//vector<string> split(const string& s) {
+//	vector<string> ret;
+//	typedef string::const_iterator iter;
+//
+//	iter i = s.begin();
+//
+//	auto isSpace = [ ](char c) { return isspace(c); };
+//	auto NotSpace = [ ](char c) { return !isspace(c); };
+//
+//	while (i != s.end()) {
+//		//공백 무시
+//		i = ::find_if(i, s.end(), NotSpace);
+//
+//		//다음단어 끝 찾기
+//		iter j = ::find_if(i, s.end(), isSpace);
+//
+//		//복사
+//		if (i != s.end()) {
+//			ret.push_back(string(i, j));
+//		}
+//
+//		i = j;
+//	}
+//
+//	return ret;
+//}
+//
+//bool is_palindrome(const string& s) {
+//	return equal(s.begin(), s.end(), s.rbegin());
+//}
+//
+//size_t width(const vector<string>& v) {
+//	size_t maxlen = 0;
+//	for (auto& str : v)
+//		maxlen = ::max(maxlen, str.size());
+//	return maxlen;
+//}
+//
+//vector<string> frame(const vector<string>& v) {
+//	vector<string> ret;
+//	size_t maxlen = width(v);
+//	string border(maxlen+4, '*');
+//
+//	ret.push_back(border); //상단 테두리 추가
+//
+//	//양끝 테두리 
+//	for (auto& s : v) {
+//		ret.push_back("* " + s + string(maxlen - s.size(), ' ')+ " *");
+//	}
+//
+//	ret.push_back(border); //하단 테두리 추가
+//	return ret;
+//}
+//
+//bool not_url_char(char c) {
+//	static const string url_ch = "~;/?:@=&$-_.+!*'(),"; //url에 들어갈 수 있는 특수 문자.
+//	return !(::isalnum(c) || ::find(url_ch.begin(), url_ch.end(), c) != url_ch.end());
+//}
+//
+//string::const_iterator url_end(string::const_iterator b, string::const_iterator e) {
+//	return ::find_if(b, e, not_url_char);
+//}
+//
+//string::const_iterator url_beg(string::const_iterator b, string::const_iterator e) {
+//	static const string sep = "://";
+//	using iter = string::const_iterator;
+//
+//	iter i = b;
+//	while ((i = ::search(i, e, sep.begin(), sep.end())) != e) { 
+//		if (i != b && i + sep.size() != e) {
+//			iter beg = i; 
+//			while (beg != b && ::isalpha(beg[-1]))
+//				--beg;
+//
+//			if (beg != i && !not_url_char(i[sep.size()]))
+//				return beg;
+//		}
+//		i += sep.size();
+//	}
+//	return e;
+//}
+//
+//vector<string> find_urls(const string& s) {
+//	vector<string> ret;
+//	using iter = string::const_iterator;
+//	//typedef string::const_iterator  iter;
+//
+//	iter b = s.begin(), e = s.end();
+//
+//	while (b != e) {
+//		b = url_beg(b, e);
+//		if (b!=e) {
+//			iter after = url_end(b, e);
+//			ret.push_back(string(b, after));
+//			b = after;
+//		}
+//	}
+//	return ret;
+//}
+//
+///*
+//    map
+//*/
+//
+//map<string, vector<int>> xref(istream& in, vector<string> find_words(const string&)) {
+//
+//	string line;
+//	int line_number = 0;
+//	map<string, vector<int> > ret;
+//
+//	//다음행 읽기
+//	while (getline(in, line)) {
+//		++line_number;
+//
+//		//주어진 행을 함수를 통해 나눔.
+//		vector<string> words = find_words(line);
+//
+//		//저장
+//		for (auto word : words)
+//			ret[word].push_back(line_number);
+//	}
+//	return ret;
+//}
 
-//반복자를 통해 split 확장성 증가시켜보기
-template < class Out>
-void split(const string& str, Out os) {
-	using iter = string::const_iterator;
-	iter i = str.begin();
+/************
+* Picture
+************/
 
-	auto isSpace = [ ](char c) { return isspace(c); };
-	auto NotSpace = [ ](char c) { return !isspace(c); };
+Picture frame(const Picture& pic) {
+	/*
+	* Pic_base* temp1 =  new Frame_Pic(pic._p);
+	* Picture temp2(temp1);
+	* 
+	* return temp2;
+	*/
+	return new Frame_Pic(pic._p);  //생성자로 인해 암묵적 conversion발생
+}
 
-	while (i != str.end()) {
+Picture vcat(const Picture& top, const Picture& bottom) {
+	return new VCat_Pic(top._p, bottom._p);
+}
 
-		i = find_if(i, str.end(), NotSpace);
+Picture hcat(const Picture& left, const Picture& right) {
+	return new HCat_Pic(left._p, right._p);
+}
 
-		iter j = find_if(i, str.end(), isSpace);
+std::ostream& operator<<(std::ostream& os, const Picture& pic) {
+	const size_t maxht = pic._p->height();
 
-		if (i != str.end())
-			*os++ = string(i, j);  //stl의 반복자 특성을 이용.
-
-		i = j;
+	for (size_t i = 0; i!= maxht; ++i) {
+		pic._p->display(os, i, false);
+		os << "\n";
 	}
+	return os;
 }
-
-vector<string> split(const string& s) {
-	vector<string> ret;
-	typedef string::const_iterator iter;
-
-	iter i = s.begin();
-
-	auto isSpace = [ ](char c) { return isspace(c); };
-	auto NotSpace = [ ](char c) { return !isspace(c); };
-
-	while (i != s.end()) {
-		//공백 무시
-		i = ::find_if(i, s.end(), NotSpace);
-
-		//다음단어 끝 찾기
-		iter j = ::find_if(i, s.end(), isSpace);
-
-		//복사
-		if (i != s.end()) {
-			ret.push_back(string(i, j));
-		}
-
-		i = j;
-	}
-
-	return ret;
-}
-
-bool is_palindrome(const string& s) {
-	return equal(s.begin(), s.end(), s.rbegin());
-}
-
-size_t width(const vector<string>& v) {
-	size_t maxlen = 0;
-	for (auto& str : v)
-		maxlen = ::max(maxlen, str.size());
-	return maxlen;
-}
-
-vector<string> frame(const vector<string>& v) {
-	vector<string> ret;
-	size_t maxlen = width(v);
-	string border(maxlen+4, '*');
-
-	ret.push_back(border); //상단 테두리 추가
-
-	//양끝 테두리 
-	for (auto& s : v) {
-		ret.push_back("* " + s + string(maxlen - s.size(), ' ')+ " *");
-	}
-
-	ret.push_back(border); //하단 테두리 추가
-	return ret;
-}
-
-bool not_url_char(char c) {
-	static const string url_ch = "~;/?:@=&$-_.+!*'(),"; //url에 들어갈 수 있는 특수 문자.
-	return !(::isalnum(c) || ::find(url_ch.begin(), url_ch.end(), c) != url_ch.end());
-}
-
-string::const_iterator url_end(string::const_iterator b, string::const_iterator e) {
-	return ::find_if(b, e, not_url_char);
-}
-
-string::const_iterator url_beg(string::const_iterator b, string::const_iterator e) {
-	static const string sep = "://";
-	using iter = string::const_iterator;
-
-	iter i = b;
-	while ((i = ::search(i, e, sep.begin(), sep.end())) != e) { 
-		if (i != b && i + sep.size() != e) {
-			iter beg = i; 
-			while (beg != b && ::isalpha(beg[-1]))
-				--beg;
-
-			if (beg != i && !not_url_char(i[sep.size()]))
-				return beg;
-		}
-		i += sep.size();
-	}
-	return e;
-}
-
-vector<string> find_urls(const string& s) {
-	vector<string> ret;
-	using iter = string::const_iterator;
-	//typedef string::const_iterator  iter;
-
-	iter b = s.begin(), e = s.end();
-
-	while (b != e) {
-		b = url_beg(b, e);
-		if (b!=e) {
-			iter after = url_end(b, e);
-			ret.push_back(string(b, after));
-			b = after;
-		}
-	}
-	return ret;
-}
-
-/*
-map
-*/
-
-map<string, vector<int>> xref(istream& in, vector<string> find_words(const string&)) {
-
-	string line;
-	int line_number = 0;
-	map<string, vector<int> > ret;
-
-	//다음행 읽기
-	while (getline(in, line)) {
-		++line_number;
-
-		//주어진 행을 함수를 통해 나눔.
-		vector<string> words = find_words(line);
-
-		//저장
-		for (auto word : words)
-			ret[word].push_back(line_number);
-	}
-	return ret;
-}
-
--- 23UOSEMBstudy_summer/Picture.h
+++ 23UOSEMBstudy_summer/Picture.h
@@ -1,26 +1,120 @@
 #pragma once
+#include "Handle.h"
 #include <vector>
 #include <string>
 #include <map>
-template < class Out>
-void split(const string& str, Out os);
+//template < class Out>
+//void split(const string& str, Out os);
+//
+//vector<string> split(const string& s); 
+//vector<string> split(const string& s); 
+//
+//size_t width(const vector<string>& v);
+//vector<string> frame(const vector<string>& v);
+//
+//vector<string> find_urls(const string& s);
+//
+//map<string, vector<int>> xref(istream& in, vector<string> find_words(const string&) = split);
 
-vector<string> split(const string& s); 
-vector<string> split(const string& s); 
-
-size_t width(const vector<string>& v);
-vector<string> frame(const vector<string>& v);
-
-vector<string> find_urls(const string& s);
-
-map<string, vector<int>> xref(istream& in, vector<string> find_words(const string&) = split);
-
+class Picture;
 
 class Pic_base {
+
+	friend std::ostream& operator<< (std::ostream& os, const Picture& pic);
+
+protected:
 	using ht_sz = std::vector<std::string>::size_type;
 	using wd_sz = std::string::size_type;
 private:
 	virtual wd_sz width() const		= 0;
 	virtual ht_sz height() const	= 0;
 	virtual void display(std::ostream&, ht_sz, bool) const = 0;
+
+public:
+	virtual ~Pic_base() { }
 };
+
+class String_Pic : public Pic_base {
+
+	friend class Picture;
+
+private:
+	String_Pic(const std::vector<string>& v) : _data(v) { }
+
+	wd_sz width() const override;
+	ht_sz height() const override;
+private:
+	std::vector<string> _data;
+};
+
+class Frame_Pic : public Pic_base {
+
+	friend class Picture;
+	friend Picture frame(const Picture& pic);
+
+private:
+	Frame_Pic(const Ptr<Pic_base>& pic) : _p(pic) { }
+
+	wd_sz width() const override;
+	ht_sz height() const override;
+	void display(std::ostream&, ht_sz, bool) const override;
+
+private:
+	Ptr<Pic_base> _p;
+};
+
+class VCat_Pic : public Pic_base {
+
+	friend class Picture;
+	friend Picture vcat(const Picture& top, const Picture& bottom);
+
+private:
+	VCat_Pic(const Ptr<Pic_base>& t , const Ptr<Pic_base>& b) : _top(t), _bottom(b) { }
+
+	wd_sz width() const override;
+	ht_sz height() const override;
+	void display(std::ostream&, ht_sz, bool) const override;
+
+private:
+	Ptr<Pic_base> _top, _bottom;
+};
+
+class HCat_Pic : public Pic_base {
+
+	friend class Picture;
+	friend Picture hcat(const Picture& left, const Picture& right);
+
+private:
+	HCat_Pic(const Ptr<Pic_base>& l, const Ptr<Pic_base>& r) : _leff(l), _right(r) { }
+
+	wd_sz width() const override;
+	ht_sz height() const override;
+	void display(std::ostream&, ht_sz, bool) const override;
+
+private:
+	Ptr<Pic_base> _leff, _right;
+};
+
+//인터페이스 클래스
+class Picture {
+
+	friend class Pic_base;
+	friend Picture frame(const Picture& pic);
+	friend Picture vcat(const Picture& top, const Picture& bottom);
+	friend Picture hcat(const Picture& left, const Picture& right);
+	friend std::ostream& operator<< (std::ostream& os, const Picture& pic);
+
+public:
+	Picture(const std::vector<string>& = std::vector<string>());
+
+private:
+	Picture(Pic_base* ptr) : _p(ptr) { }
+
+private:
+	Ptr<Pic_base> _p;
+};
+
+Picture frame(const Picture& pic);
+Picture vcat(const Picture& top, const Picture& bottom);
+Picture hcat(const Picture& left, const Picture& right);
+std::ostream& operator<< (std::ostream& os, const Picture& pic);
