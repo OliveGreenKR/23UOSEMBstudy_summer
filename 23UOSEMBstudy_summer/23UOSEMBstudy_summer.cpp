2567d49f6d86b97959cb76a674de42940b61183f
OliveGreenKR <jeongchul0098@gmail.com>
2023-07-17 12:18:14 +0900

    generic print

--- 23UOSEMBstudy_summer/23UOSEMBstudy_summer.cpp
+++ 23UOSEMBstudy_summer/23UOSEMBstudy_summer.cpp
@@ -2,47 +2,42 @@
 #include <iomanip>  //setprecision
 #include <ios>      //streamsize
 #include <string>
-
+#include <ostream>
 using namespace std;
 //using std::cin;
+
+
+template <typename T>
+void print(T arg) {
+    std::cout << arg << std::endl;
+}
+
+template <typename T, typename... Types>
+void print(T arg, Types... args) {
+    std::cout << arg << ", ";
+    print(args...);
+}
+
+template<>
+void print(double arg) {
+    streamsize prec = cout.precision();
+    std::cout << setprecision(3) << arg << setprecision(prec) << std::endl;
+}
+
+template <typename , typename... Types>
+void print(double arg, Types... args) {
+    streamsize prec = cout.precision();
+    std::cout << setprecision(3) << arg << setprecision(prec) << ", ";
+    print(args...);
+}
 
 
 int main()
 {
     ios_base::sync_with_stdio(false); //FASTIO , dont use printf,scanf anymore
+    double d = 3.5555;
+	print("abc", 3.112313553, 1, d, 2, 3, 4, "jjjj");
 
-    // 정보 입력
-    string name;
-    cout << "enter your first name : ";
-    cin >> name; 
-
-    double midterm, final;
-    cout << "Enter your midterm and final exam grades : ";
-    cin >> midterm >> final;
-
-    double homework;
-    double sum_hw = 0;
-    int cnt_hw = 0;
-    cout << "Enter your howework grades , followd by EOF.\n";
-
-    while (cin >> homework) {
-        ++cnt_hw;
-        sum_hw += homework;
-    }
-   
-    streamsize prec = cout.precision();
-    cout << "yout final grade is " << setprecision(3)
-        << 0.2 * midterm + 0.4* final + 0.4*(sum_hw / cnt_hw) << setprecision(prec) << endl;
-
-    /* 
-    streamsize prec = cout.precision(3);   
-    cout << "yout final grade is " << setprecision(3)
-         << 0.2 * midterm + 0.4* final + 0.4*(sum_hw / cnt_hw) << setprecision(prec) << endl;
-    cout.precision(prec);
-    
-    이렇게도 가능하지만, 위의 방법을 추천.
-    why? 아래의 방법은 cout 전체에 적용 -> 프로그램 전체에 영향을 미치므로 위험하다.
-    */
 }
 
 
